61f5e49061e681e3120d76296b9fa5c6bce9d254
otumian <popecan1000@gmail.com>
2019-08-29 10:00:59 +0000

    makeprofit.py - modified

--- makeprofit.py
+++ makeprofit.py
@@ -1,15 +1,18 @@
-"""
-To make a profit, a local store marks up the prices of its items by a certain
-percentage. Write a C++ program that reads the original price of the item sold, 
-the percentage of the marked-up price, and the sales tax rate. The program then
-outputs the original price of the item, the percentage of the mark-up, the store’s
-selling price of the item, the sales tax rate, the sales tax, and the final price of the
-item. (The final price of the item is the selling price plus the sales tax.)
-"""
-
-# input: original price, percentage of mark-up price, sales tax rate
-# output: original price, percentage of the mark-up, selling price and sales tax rate
-# final price = selling price + sales tax
+# author: Otumian (github.com/otumian-empire)
+# Description: 
+# 	To make a profit, a local store marks up the prices of its items 
+# 	by a certain percentage. Write a C++ program that reads the original 
+# 	price of the item sold, 
+# 	the percentage of the marked-up price, and the sales tax rate. 
+# 	The program then outputs the original price of the item, the percentage 
+# 	of the mark-up, the store’s selling price of the item, 
+# 	the sales tax rate, the sales tax, and the final price of the item. 
+# 	(The final price of the item is the selling price plus the sales tax.)
+# Algorithm:
+# 	input: original price, percentage of mark-up price, sales tax rate
+# 	output: original price, percentage of the mark-up, selling price and sales
+#	tax rate
+# 	final price = selling price + sales tax
 
 originalprice = float(input("Enter original price of the item sold: "))
 profitpercentage = float(input("Enter profit ppercentage: "))
@@ -26,3 +29,4 @@ print("The tax on the sales is %.2f" %salestax)
 
 finalprice = sellingprice + salestax
 print("The final cost then becomes %.2f %x" %finalprice %finalprice)
+
