14afe4234af993dcb630df514181753212fa3ef9
otumian <popecan1000@gmail.com>
2019-08-29 07:34:31 +0000

    averagetestscores.py - modified: made readble

--- averagetestscores.py
+++ averagetestscores.py
@@ -1,9 +1,11 @@
 """
-Write a program that prompts the user to enter five test scores and then prints
+Write a program that prompts the user to enter five test scores and then
+ prints
 the average test score. (Assume that the test scores are decimal numbers.)
 """
 
-firstnumber = secondnumber = thirdnumber = fourthnumber = fifthnumber = sumofnumbers = averageofthenumbers = 0
+firstnumber = secondnumber = thirdnumber = 0
+fourthnumber = fifthnumber = sumofnumbers = averageofthenumbers = 0
 
 firstnumber = float(input('Enter the first number: '))
 secondnumber = float(input('Enter the second number: '))
@@ -11,7 +13,8 @@ thirdnumber = float(input('Enter the third number: '))
 fourthnumber = float(input('Enter the fourth number: '))
 fifthnumber = float(input('Enter the fifth number: '))
 
-sumofnumbers = firstnumber + secondnumber + thirdnumber + fourthnumber + fifthnumber
+sumofnumbers = firstnumber + secondnumber + thirdnumber + fourthnumber 
+sumofnumbers += fifthnumber
 
 averageofthenumbers = sumofnumbers / 5.0
 
